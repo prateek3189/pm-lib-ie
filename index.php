4b2057d5bbf01a3c8f1b2f62338ae9324bfe2a3a
pmagarde <prateekmag@cybage.com>
2015-04-21 13:05:31 +0530

    IE8 specific

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE>
+<html>
+<head>
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="pm-lib-min.js"></script>
+<link type="text/css" rel="stylesheet" href="pm-lib-min.css"/>
+</head>
+<body>
+<select id="S1" class="custom">
+	<option value="">Select</option>
+	<option value="V11" >Val11</option>
+	<option value="V12">Val12</option>
+	<option value="V13">Val13</option>
+	<option value="V14"selected="selected">Val14</option>
+</select>
+<br/>
+<select id="S2">
+	<option value="">Select</option>
+	<option value="V11">Val11</option>
+	<option value="V12">Val12</option>
+	<option value="V13" selected="selected">Val13</option>
+	<option value="V14">Val14</option>
+</select>
+
+
+
+</body>
+</html>
